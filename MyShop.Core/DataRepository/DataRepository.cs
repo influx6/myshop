9676299f465433fd830bbe31e9c76ca50612066f
Alexander <trinoxf@gmail.com>
2018-04-29 22:10:14 +0100

    Add DataRepository

---
+++ MyShop.Core/DataRepository/DataRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.DataRepository
+{
+    public interface DataRepository<T>
+    {
+        void Save(T t);
+        IQueryable<T> List();
+         T Get(Predicate<T> match);
+        void Delete(Predicate<T> match);
+        void Update(Predicate<T> match, T t);
+    }
+}

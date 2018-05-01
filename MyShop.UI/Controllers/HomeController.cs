cf218358db262f96fd768828a824ee442a31f90b
Alexander <trinoxf@gmail.com>
2018-05-01 16:42:47 +0100

    Change homepage to product page

--- MyShop.UI/Controllers/HomeController.cs
+++ MyShop.UI/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MyShop.Core.DataRepository;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,31 @@ namespace MyShop.UI.Controllers
 {
     public class HomeController : Controller
     {
+
+        IDataRepository<Product> productsCtx;
+        IDataRepository<ProductCategory> categories;
+
+        public HomeController(IDataRepository<Product> products, IDataRepository<ProductCategory> categories)
+        {
+            this.productsCtx = products;
+            this.categories = categories;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            return View(this.productsCtx.List());
+        }
+
+        public ActionResult Detail(string id)
+        {
+            try
+            {
+                Product p = this.productsCtx.Get(id);
+                return View(p);
+            }
+            catch { 
+                return HttpNotFound();
+            }
         }
 
         public ActionResult About()
